70fa6b2e6ac74dea6b72c2e2ec2f257c4570e2bc
Anthony Durén <anthonydadivasduren@gmail.com>
2025-04-24 09:01:22 +0200

    Add enhanced input handling for WarriorHeroCharacter
    Integrated new input system using EnhancedInputSubsystem for movement and camera controls. Implemented input actions for movement (Input_Move) and looking (Input_Look), and setup input bindings in SetupPlayerInputComponent. Adjusted class properties to support the new input framework.

--- Source/RPG_Combat_Project/Public/Characters/WarriorHeroCharacter.h
+++ Source/RPG_Combat_Project/Public/Characters/WarriorHeroCharacter.h
@@ -6,12 +6,15 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-class USpringArmComponent;
-class UCameraComponent;
-
 /**
  * 
  */
+
+class USpringArmComponent;
+class UCameraComponent;
+class UDataAsset_InputConfig;
+struct FInputActionValue;
+
 UCLASS()
 class RPG_COMBAT_PROJECT_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
@@ -21,16 +24,29 @@ public:
 	AWarriorHeroCharacter();
 	
 protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
 	virtual void BeginPlay() override;
 
 private:
 #pragma region Components
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
 
 #pragma endregion
+
 	
+
+#pragma region Inputs
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+	
+#pragma endregion
 };
