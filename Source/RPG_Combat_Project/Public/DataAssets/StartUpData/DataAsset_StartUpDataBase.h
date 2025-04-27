246cb5ad27451bebcecdce223be47c7712c92e7f
Anthony Durén <anthonydadivasduren@gmail.com>
2025-04-28 00:24:55 +0200

    "Add Hero startup data assets and ability system integration"

---
+++ Source/RPG_Combat_Project/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_StartUpDataBase.generated.h"
+
+/**
+ * 
+ */
+
+class UWarriorGameplayAbility;
+class UWarriorAbilitySystemComponent;
+UCLASS()
+class RPG_COMBAT_PROJECT_API UDataAsset_StartUpDataBase : public UDataAsset
+{
+	GENERATED_BODY()
+
+	public:
+	virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel = 1) const;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray< TSubclassOf< UWarriorGameplayAbility > > ActivateOnGivenAbilities ;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray< TSubclassOf < UWarriorGameplayAbility > > ReactiveAbilities ;
+
+	void GrantAbilities(const TArray< TSubclassOf< UWarriorGameplayAbility > >& InAbilitiesToGive, UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel = 1) const;
+	
+};
